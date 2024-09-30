ce503bb975d6ea52b130faef425052aafae47a74
sujal <sujalmakkar1111@gmail.com>
2024-09-30 13:47:49 +0530

    version 1

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+
 const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
 
